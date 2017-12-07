eca0b2f17e4c6ff17819ed66958e5cdd704f99f0
Ryan McLeod <ryanmcleod@gmail.com>
2017-12-06 16:47:44 -0800

    init

--- LargeLabelAccessibilityTest/ViewController.swift
+++ LargeLabelAccessibilityTest/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var elPequeno: UILabel!
+    @IBOutlet weak var elGordo: UILabel!
+    @IBOutlet weak var debugButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        stackView.distribution = .fillEqually
+
+        let rowCount = 20
+        for index in 1...rowCount {
+            let label = UILabel()
+            label.text = "\(index)"
+            label.font = UIFont.systemFont(ofSize: 18)
+            label.backgroundColor = UIColor.black.withAlphaComponent(CGFloat(index) / CGFloat(rowCount))
+            stackView.addArrangedSubview(label)
+        }
+
+        elPequeno.text = "X"
+        elPequeno.backgroundColor = UIColor.black.withAlphaComponent(0.1)
+        elPequeno.font = UIFont.boldSystemFont(ofSize: 80)
+
+        elGordo.text = "X"
+        elGordo.backgroundColor = UIColor.black.withAlphaComponent(0.1)
+        elGordo.font = UIFont.boldSystemFont(ofSize: 200)
+
+        autoSetAccessibilityElements()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func debugAction() {
+        if (view.accessibilityElements == nil) {
+            manuallySetAccessibilityElements()
+        } else {
+            autoSetAccessibilityElements()
+        }
     }
 
+    func manuallySetAccessibilityElements() {
 
+        view.accessibilityElements = [elPequeno, elGordo, debugButton] + stackView.arrangedSubviews
+//        view.accessibilityElements =  stackView.arrangedSubviews + [elPequeno, elGordo, debugButton] // Reversed
+        debugButton.setTitle("set accessibilityElements to nil (auto)", for: .normal)
+        debugButton.setTitleColor(.blue, for: .normal)
+    }
+
+    func autoSetAccessibilityElements() {
+        view.accessibilityElements = nil
+        debugButton.setTitle("set accessibilityElements manually", for: .normal)
+        debugButton.setTitleColor(.red, for: .normal)
+    }
 }
 
